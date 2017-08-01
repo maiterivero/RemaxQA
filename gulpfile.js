25ba319f1027a3a250998b31504fe0fa58b75611
maiterivero <maite.rivero@homes.com>
2017-08-01 10:22:03 -0400

    Update gulpfile.js
    delete dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,6 @@ const gulp = require('gulp');
 const setUpDriver = require('./setUpTest');
 const jasmine = require('gulp-jasmine');
 const reporters = require('jasmine-reporters'); 
-var screenShotReporter = require('jasmine-screenshot-reporter');
- var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 gulp.task('default', () =>    
     gulp.src('spec/*.spec.js')
         // gulp-jasmine works on filepaths so you can't have any plugins before it 
