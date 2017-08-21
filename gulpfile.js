c06ea4e6af927c761614cbd498f6a9390c6ee238
maiterivero <maite.rivero@homes.com>
2017-08-21 17:44:22 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@
 const gulp = require('gulp');
 const setUpDriver = require('./setUpTest');
 const jasmine = require('gulp-jasmine');
-const reporters = require('jasmine-reporters'); 
+const reporters = require('./html_reporter.js');
 gulp.task('default', () =>    
     gulp.src('spec/*.spec.js')
         // gulp-jasmine works on filepaths so you can't have any plugins before it 
         .pipe(jasmine({
-            //reporter: new reporters.HTMLReporter()
+            reporter: new reporters.HTMLReporter()
            
         }))
        
